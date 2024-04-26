e4e8792ace6de722ee8d145d5ea97be94674542b
rhwf22 <150405419+rhwf22@users.noreply.github.com>
2024-04-25 17:48:27 -0700

    show characters typed

--- JS.js
+++ JS.js
@@ -1,34 +1,43 @@
 var scentence = "ray is a monkey and this is his type test";
+var keysTyped = ""
 var lettersTyped = 0;
 var averageLength = 0;
 var started = false;
 var done = false;
-const startTime = new Date();
+var startTime = new Date();
 var endTime = new Date();
-
+console.log(startTime)
 // document.addEventListener("load", ()=>{
 //     console.log("loaded");
 //     startTime.getTime();
 // })
 window.addEventListener("keydown", (event)=>{
     if (started == false) {
-        startTime.getTime();
+        startTime = Date.now();
         started = true;
+        console.log('started');
+        console.log(startTime);
     }
     typing(event.key);
 })
 function typing(key) {
-    lettersTyped++;
+    if (key.length == 1) {
+        lettersTyped++;
+        keysTyped = keysTyped + key;
+        let characterstyped = document.getElementById("characters").innerHTML = "Characters typed: " + lettersTyped;
+        let userInput = document.getElementById("scentencetyped").innerHTML = keysTyped;
+    }
     if (lettersTyped >= scentence.length) {
         finished();
     }
 }
 function finished() {
     if (done)  {return}
-    averageLength = (scentence.length - 9) / 10;
-    console.log(averageLength);
+    averageLength = (scentence.length) / 10;
     endTime = Date.now();
+    console.log(endTime);
     let timeTaken = endTime - startTime;
+    console.log(timeTaken);
     var wpm = Math.floor((((lettersTyped / averageLength) / timeTaken) * 60) * 1000);
     let wordperminute = document.getElementById("wpm").innerHTML = "WPM: " + wpm;
     console.log(timeTaken);
